c67aeb2b8521182126a4e125290989669cc76a99
Harald Welte <laforge@osmocom.org>
2020-12-15 18:35:42 +0100

    icE1usb fw: Use green per-port LED to indicate alignment status
    Whenever Rx is aligned, the green LED is permanently on.
    Whenever Rx is not aligned, the green LED is blinking.
    
    What's missing is to check for E1 clock ticks and turn the LED off
    completely if there are no clock ticks.
    
    Change-Id: I42d53544858dbbbae5206d9a62b08672966c9ebf

--- firmware/ice40-riscv/icE1usb/e1.h
+++ firmware/ice40-riscv/icE1usb/e1.h
@@ -13,3 +13,19 @@ void e1_debug_print(bool data);
 
 volatile uint8_t *e1_data_ptr(int mf, int frame, int ts);
 unsigned int e1_data_ofs(int mf, int frame, int ts);
+
+enum e1_platform_led {
+	E1P_LED_GREEN		= 0,
+	E1P_LED_YELLOW		= 1,
+};
+
+enum e1_platform_led_state {
+	E1P_LED_ST_OFF		= 0,
+	E1P_LED_ST_ON		= 1,
+	E1P_LED_ST_BLINK	= 2,
+	E1P_LED_ST_BLINK_FAST	= 3
+};
+
+/* external function provided by the platform; used by E1 driver to control LEDs */
+extern void e1_platform_led_set(uint8_t port, enum e1_platform_led led,
+				enum e1_platform_led_state state);
--- firmware/ice40-riscv/icE1usb/misc.c
+++ firmware/ice40-riscv/icE1usb/misc.c
@@ -10,6 +10,7 @@
 
 #include "config.h"
 #include "misc.h"
+#include "e1.h"
 
 
 struct misc {
@@ -49,6 +50,24 @@ void
 e1_led_set(bool enable, uint8_t cfg)
 {
 	misc_regs->e1_led = (enable ? 0x100 : 0x000) | cfg;
+}
+
+void
+e1_platform_led_set(uint8_t port, enum e1_platform_led led,
+		    enum e1_platform_led_state state)
+{
+	uint32_t tmp;
+	unsigned int shift;
+
+	if (port >= 2)
+		return;
+
+	shift = 4*port + 2*led;
+
+	tmp = misc_regs->e1_led;
+	tmp &= ~(3 << shift);
+	tmp |= 0x100 | ((state & 3) << shift);
+	misc_regs->e1_led = tmp;
 }
 
 uint16_t
