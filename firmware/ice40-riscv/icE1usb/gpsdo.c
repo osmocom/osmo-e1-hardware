osmocom/osmo-e1-hardware

commit da395cc9224f57300d8b05b8369f08d202f5c184

icE1usb fw/gpsdo: If no PPS present for >= 3s, go to hold over mode
Signed-off-by: Sylvain Munaut <tnt@246tNt.com>
Change-Id: Ia85a8bb0e146cb117ea6e2704c6a4dedf215c75a

firmware/ice40-riscv/icE1usb/gpsdo.c
@@ -15,6 +15,8 @@
 
 #include "ice1usb_proto.h"
 
+#include "config.h"
+
 
 struct {
 	/* Configuration */
@@ -275,6 +277,14 @@ gpsdo_poll(void)
 	uint32_t tick_now, tick_diff;
 	bool valid;
 
+	/* If more than 3 sec elapsed since last PPS, go to hold-over */
+	if (time_elapsed(g_gpsdo.meas.tick_prev, 3 * SYS_CLK_FREQ)) {
+		g_gpsdo.state = STATE_HOLD_OVER;
+		g_gpsdo.meas.invalid = 0;
+		g_gpsdo.meas.skip = 0;
+		return;
+	}
+
 	/* Get current tick and check if there was a PPS and estimate frequency */
 	tick_now = time_pps_read();
 
@@ -299,6 +309,7 @@ gpsdo_poll(void)
 		/* No GPS fix, go to hold-over */
 		g_gpsdo.state = STATE_HOLD_OVER;
 		g_gpsdo.meas.invalid = 0;
+		g_gpsdo.meas.skip = 0;
 		return;
 	}
 

commit 166d49d7853d4be43c0c4b97e019eb8f18554559

icE1usb fw/gpsdo: Only force HOLD_OVER when no PPS if not DISABLED
Bug introduced in da395cc9224f57300d8b05b8369f08d202f5c184
but since we never had anyway to use manual mode before,
didn't get noticed ...

Signed-off-by: Sylvain Munaut <tnt@246tNt.com>
Change-Id: I6e2b6cf5c7c0c9deaf429be000770316ec88b9d4

firmware/ice40-riscv/icE1usb/gpsdo.c
@@ -278,7 +278,8 @@ gpsdo_poll(void)
 	bool valid;
 
 	/* If more than 3 sec elapsed since last PPS, go to hold-over */
-	if (time_elapsed(g_gpsdo.meas.tick_prev, 3 * SYS_CLK_FREQ)) {
+	if ((g_gpsdo.state != STATE_DISABLED) &&
+	    time_elapsed(g_gpsdo.meas.tick_prev, 3 * SYS_CLK_FREQ)) {
 		g_gpsdo.state = STATE_HOLD_OVER;
 		g_gpsdo.meas.invalid = 0;
 		g_gpsdo.meas.skip = 0;